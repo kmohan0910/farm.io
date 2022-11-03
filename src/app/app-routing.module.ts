7a2ed7edae2b1a38c26448d8780957e0e9982816
Mohan <kmohan0910@gmail.com>
2022-11-03 21:50:12 +0530

    Added Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponentComponent } from './HomeComponent/home-component/home-component.component';
+import { Signup } from './SignupComponent/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: "login", component: Signup },
+{path: "", component: HomeComponentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Signup } from './SignupComponent/signup.component';
+import { HeaderComponentComponent } from './shared/header-component/header-component.component';
+import { HomeComponentComponent } from './HomeComponent/home-component/home-component.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     Signup,
+    HeaderComponentComponent,
+    HomeComponentComponent,
   ],
   imports: [
     BrowserModule,
